97612c47cff21b1499007579811d2bca7a984357
zazasj <zazang0503@naver.com>
2023-11-08 16:52:02 +0900

    update1

--- src/main/java/dev/mvc/team5/DatabaseConfiguration.java
+++ src/main/java/dev/mvc/team5/DatabaseConfiguration.java
@@ -2,6 +2,8 @@ package dev.mvc.team5;
 
 import javax.sql.DataSource;
 
+import javax.sql.DataSource;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -16,36 +18,39 @@ import org.springframework.context.annotation.PropertySource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+@Configuration
+@PropertySource("classpath:/application.properties")  // 설정 파일 위치
+@MapperScan(basePackages= {}) // DAO interface를 찾는 위치
 public class DatabaseConfiguration {
-
-  @Autowired
-  private ApplicationContext applicationContext;
-  
-  @Bean
-  @ConfigurationProperties(prefix="spring.datasource.hikari")  // 설정 파일의 접두사 선언 spring.datasource.hikari....
-  public HikariConfig hikariConfig() {
-      return new HikariConfig();
-  }
-  
-  @Bean
-  public DataSource dataSource() throws Exception{
-      DataSource dataSource = new HikariDataSource(hikariConfig());
-      System.out.println(dataSource.toString());  // 정상적으로 연결 되었는지 해시코드로 확인
-      return dataSource;
-  }
-  
-  @Bean
-  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
-      SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-      sqlSessionFactoryBean.setDataSource(dataSource);
-      // "/src/main/resources/mybatis" 폴더의 파일명이 "xml"로 끝나는 파일 매핑
-      sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/**/*.xml"));
-      
-      return sqlSessionFactoryBean.getObject();
-  }
-  
-  @Bean
-  public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
-      return new SqlSessionTemplate(sqlSessionFactory);
-  }
+    
+    @Autowired
+    private ApplicationContext applicationContext;
+    
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource.hikari")  // 설정 파일의 접두사 선언 spring.datasource.hikari....
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+    
+    @Bean
+    public DataSource dataSource() throws Exception{
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        System.out.println(dataSource.toString());  // 정상적으로 연결 되었는지 해시코드로 확인
+        return dataSource;
+    }
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        // "/src/main/resources/mybatis" 폴더의 파일명이 "xml"로 끝나는 파일 매핑
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/**/*.xml"));
+        
+        return sqlSessionFactoryBean.getObject();
+    }
+    
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
